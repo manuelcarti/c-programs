dfcad1f7bc5b85141c68ee3194ced6fc2bdaf926
manuelcarti <nuelvamp@gmail.com>
2024-09-30 16:25:35 +0300

    Add files via upload

---
+++ function to calculate fine amount.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+// Function to calculate the fine amount
+int calculateFine(int daysOverdue) {
+    if (daysOverdue <= 7) {
+        return 20 * daysOverdue;
+    } else if (daysOverdue <= 14) {
+        return 50 * daysOverdue;
+    } else {
+        return 100 * daysOverdue;
+    }
+}
+
+int main() {
+    int bookID;
+    int dueDate, returnDate;
+    int daysOverdue;
+    int fineAmount, fineRate;
+
+    
+    printf("Enter book ID: ");
+    scanf("%d", &bookID);
+
+    printf("Enter due date: ");
+    scanf("%d", &dueDate);
+
+    printf("Enter return date: ");
+    scanf("%d", &returnDate);
+
+    
+    daysOverdue = returnDate - dueDate;
+
+    if (daysOverdue > 0) {
+        
+        fineAmount = calculateFine(daysOverdue);
+
+        if (daysOverdue <= 7) {
+            fineRate = 50;
+        } else if (daysOverdue <= 14) {
+            fineRate = 30;
+        } else {
+            fineRate = 100;
+        }
+    } else {
+        fineAmount = 0;
+        fineRate = 0;
+        daysOverdue = 0; 
+    }
+
+    
+    printf("\nBook ID: %d\n", bookID);
+    printf("Due Date: %d\n", dueDate);
+    printf("Return Date: %d\n", returnDate);
+    printf("Days Overdue: %d\n", daysOverdue);
+    printf("Fine Rate: Ksh. %d per day\n", fineRate);
+    printf("Fine Amount: Ksh. %d\n", fineAmount);
+
+    return 0;
+}

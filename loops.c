d4742175f7c64c06648b3a0acddc2d3602b655fa
manuelcarti <nuelvamp@gmail.com>
2024-10-07 11:06:03 +0300

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,32 @@
+// for loops
+#include <stdio.h>
+
+int main() {
+    int k;
+    printf("enter the number of terms");
+    scanf("%d",&k);
+    
+    for(k=1;k<=20;k++){
+   
+    
+    }
+   printf("the number is:%d and cube of %d:%d\n",k,k,k*k*k);
+    
+    
+
+    return 0;
+}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+
